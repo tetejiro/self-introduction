c717a262dbf6048326f77e7d41ce9c5d940271dd
tetejiro <hy1733505@gmail.com>
2022-11-24 01:27:13 +0900

    セクションの中身の追記

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ window.addEventListener('DOMContentLoaded', function() {
 window.addEventListener('scroll', function() {
   // section1
   if($(this).scrollTop() < 600) {
-    $('section').css('background', '#709ed0');
-    $('nav').css('background', 'rgba(181,203,197,0.6)');
+    $('section').css('background', '#c6ccdf');
+    $('nav').css('background', '#c6ccdf');
     $('li').each(function(idx, val) {
       if(idx > 0) {
         val.style.border = 'none';
       }
     });
-    $('li.list-home').css('border-bottom', 'white solid 2px');
+    $('li.list-home').css('border-bottom', '2px solid white');
   // section2
   } else if(600 < $(this).scrollTop() && $(this).scrollTop() < 1250) {
-    $('section').css('background', '#acbedd');
-    $('nav').css('background', '#acbedd');
+    $('section').css('background', '#dfe2e3');
+    $('nav').css('background', '#dfe2e3');
     $('li').each(function(idx, val) {
       if(idx > 1) {
         val.style.border = 'none';
@@ -25,8 +25,9 @@ window.addEventListener('scroll', function() {
     });
     $('li.list-section2').css('border-bottom', 'white solid 2px');
   }
+
   // section3
-  if(1250 < $(this).scrollTop() && $(this).scrollTop() < 1900) {
+  else if(1250 < $(this).scrollTop() && $(this).scrollTop() < 1900) {
     $('section').css('background', '#f5d8c8');
     $('nav').css('background', '#f5d8c8');
     $('li').each(function(idx, val) {
@@ -35,22 +36,27 @@ window.addEventListener('scroll', function() {
       }
     });
     $('li.list-section3').css('border-bottom', 'white solid 2px');
+
   // section4
   } else if(1900 < $(this).scrollTop() && $(this).scrollTop() < 2550) {
-    $('section').css('background', '#dfe2e3');
-    $('nav').css('background', '#dfe2e3');
+    $('section').css('background', '#acbedd');
+    $('nav').css('background', '#acbedd');
     $('li').each(function(idx, val) {
       if(idx > 3) {
         val.style.border = 'none';
       }
     });
     $('li.list-section4').css('border-bottom', 'white solid 2px');
+
   // section5
   } else if(2550 < $(this).scrollTop()) {
-    $('section').css('background', '#c6ccdf');
-    $('nav').css('background', '#c6ccdf');
+    $('section').css('background', '#709ed0');
+    $('nav').css('background', 'rgba(181,203,197,0.6)');
     $('li').each(function(idx, val) {
-      $(val).css('border-bottom', '2px solid white');
+      if(idx > 4) {
+        val.style.border = 'none';
+      }
     });
+    $('li.list-section5').css('border-bottom', 'white solid 2px');
   }
 });
