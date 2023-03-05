1a3c4cd7e676f972312105681d560d2b41f7c176
tetejiro <hy1733505@gmail.com>
2023-03-05 22:23:21 +0900

    バグ改修・タグ修正・書き方の最適化

--- script.js
+++ script.js
@@ -1,140 +1,88 @@
-/* スクロール時の背景色・ナビの下線を付ける */
+let bg_color;
+let idx;
 
-window.addEventListener('scroll', function() {
-  // section1
-  if($(this).scrollTop() < 600) {
-    // 背景色の変更
-    $('section').css('background', '#c6ccdf');
-    // 該当のナビの下線を付ける
-    $('nav').css('background', '#c6ccdf');
-    $('li').each(function(idx, val) {
-      if(idx > 0) {
-        val.style.border = 'none';
-      }
-    });
-    $('li.list-section1').css('border-bottom', '2px solid white');
+$(window).on('DOMContentLoaded', function(){
 
-  // section2
-  } else if(600 < $(this).scrollTop() && $(this).scrollTop() < 1250) {
-    $('section').css('background', '#dfe2e3');
-    $('nav').css('background', '#dfe2e3');
-    $('li').each(function(idx, val) {
-      if(idx > 1) {
-        val.style.border = 'none';
-      }
-    });
-    $('li.list-section2').css('border-bottom', 'white solid 2px');
-  }
+  /* 初期表示時点での色の適用 */
+  changeColor();
 
-  // section3
-  else if(1250 < $(this).scrollTop() && $(this).scrollTop() < 1900) {
-    $('section').css('background', '#f5d8c8');
-    $('nav').css('background', '#f5d8c8');
-    $('li').each(function(idx, val) {
-      if(idx > 2) {
-        val.style.border = 'none';
-      }
+  /* ナビ押下時：自動スクロール機能・ナビの下線を付ける */
+  $(function(){
+    let list_y=[50, 645, 1300, 1960, 2800];
+    $('.list-section').on('click', function() {
+      let index = $('.list-section').index($(this));
+      scrollTo(0, list_y[index]);
+      $('.list-section').css({'border-bottom':'solid 0px'});
+      $(this).css({'border-bottom':'solid 2px'});
     });
-    $('li.list-section3').css('border-bottom', 'white solid 2px');
+  });
 
-  // section4
-  } else if(1900 < $(this).scrollTop() && $(this).scrollTop() < 2550) {
-    $('section').css('background', '#acbedd');
-    $('nav').css('background', '#acbedd');
-    $('li').each(function(idx, val) {
-      if(idx > 3) {
-        val.style.border = 'none';
-      }
-    });
-    $('li.list-section4').css('border-bottom', 'white solid 2px');
-
-  // section5
-  } else if(2550 < $(this).scrollTop()) {
-    $('section').css('background', '#709ed0');
-    $('nav').css('background', 'rgba(181,203,197,0.6)');
-    $('li').each(function(idx, val) {
-      if(idx > 4) {
-        val.style.border = 'none';
-      }
-    });
-    $('li.list-section5').css('border-bottom', 'white solid 2px');
-  }
 });
 
-/* 自動スクロール機能・ナビの下線を付ける */
+$(document).on('scroll', function() {
 
-window.addEventListener('DOMContentLoaded', function() {
-  let moveTop;
-  $('.list-section1').on('click', function() {
-    // 該当のポイントまで遷移させる
-    moveTop = ($('.section1').offset().top);
-    scrollTo(0, moveTop);
-    // 該当のナビの下線を付ける
-    $('li').css('border-bottom', 'white solid 2px');
-    $('li').each(function(idx, val) {
-      if(idx > 0) {
-        val.style.border = 'none';
-      }
-    });
-  });
+  /* スクロール時点での色の適用 */
+  changeColor();
 
-  $('.list-section2').on('click', function() {
-    moveTop = ($('.section2').offset().top);
-    scrollTo(0, moveTop);
-    $('li').css('border-bottom', 'white solid 2px');
-    $('li').each(function(idx, val) {
-      if(idx > 1) {
-        val.style.border = 'none';
-      }
-    });
-  });
-
-  $('.list-section3').on('click', function() {
-    moveTop = ($('.section3').offset().top);
-    scrollTo(0, moveTop);
-    $('li').css('border-bottom', 'white solid 2px');
-    $('li').each(function(idx, val) {
-      if(idx > 2) {
-        val.style.border = 'none';
-      }
-    });
-  });
-
-  $('.list-section4').on('click', function() {
-    moveTop = ($('.section4').offset().top);
-    scrollTo(0, moveTop);
-    $('li').css('border-bottom', 'white solid 2px');
-    $('li').each(function(idx, val) {
-      if(idx > 3) {
-        val.style.border = 'none';
-      }
-    });
-  });
-
-  $('.list-section5').on('click', function() {
-    moveTop = ($('.section5').offset().top);
-    scrollTo(0, moveTop);
-    $('li').css('border-bottom', 'white solid 2px');
-    $('li').each(function(idx, val) {
-      if(idx > 4) {
-        val.style.border = 'none';
-      }
-    });
-  });
-});
-
-
-// works のアニメーションクラス付与
-window.addEventListener('scroll', function() {
+  /* works のアニメーションクラス付与 */
   if($(this).scrollTop() < 1500) {
-    $('.section4 > p').removeClass("turning");
+    $('.works-title').removeClass("turning");
   }
   if(1500 < $(this).scrollTop() && $(this).scrollTop() < 2200) {
-    if(!$('.section4 > p').hasClass('turning')) {
-      $('.section4 > p').addClass("turning");
+    if(!$('.works-title').hasClass('turning')) {
+      $('.works-title').addClass("turning");
     }
   }
   if(2200 < $(this).scrollTop()) {
-    $('.section4 > p').removeClass("turning");
+    $('.works-title').removeClass("turning");
   }
+
 });
+
+
+/* 色・下線の適用処理 */
+function changeColor () {
+  /* 位置による適切な色の取得 */
+  $.each($('section'), () => {
+
+    // section1
+    if(0 < $(this).scrollTop() && $(this).scrollTop() < 600) {
+      bg_color = '#c6ccdf';
+      idx = 0;
+    }
+
+    // section2
+    else if(600 < $(this).scrollTop() && $(this).scrollTop() < 1250) {
+      bg_color = '#dfe2e3';
+      idx = 1;
+    }
+
+    // section3
+    else if(1250 < $(this).scrollTop() && $(this).scrollTop() < 1900) {
+      bg_color = '#f5d8c8';
+      idx = 2;
+    }
+
+    // section4
+    else if(1900 < $(this).scrollTop() && $(this).scrollTop() < 2550) {
+      bg_color =  '#acbedd';
+      idx = 3;
+    }
+
+    // section5
+    else if(2550 < $(this).scrollTop()) {
+      bg_color =  '#709ed0';
+      idx = 4;
+    }
+
+    // 取得した色を適用
+    $('section').css('background', bg_color); // 背景色
+    $('nav').css('background', bg_color); // ナビ背景色
+
+    // 下線
+    $('li').css('border-bottom', 'none'); // 外す
+    for(let i=0; i<=idx; i++) {
+      $('li')[i].style.borderBottom = 'white solid 2px'; // 付ける
+    }
+  });
+}
